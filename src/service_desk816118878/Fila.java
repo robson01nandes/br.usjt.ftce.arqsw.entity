34c88fb0d2d2d17761d23d051b2eaa540ef3b35f
Robson <Robson@192.168.0.112>
2018-02-23 13:45:54 -0300

    teste

---
+++ src/service_desk816118878/Fila.java
@@ -0,0 +1,20 @@
+package service_desk816118878;
+
+public class Fila {
+	private int id;
+	private String nome;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
